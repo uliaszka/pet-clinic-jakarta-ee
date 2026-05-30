34d933285250455296e595dc142aa0f7b1d9bb00
Uliana Morozova <uliana.morozova@student.um.si>
2026-05-31 00:17:54 +0200

    refactor Pet model and introduce Dao interface

--- src/main/java/com/uliana/petclinic/dao/InMemoryPetDao.java
+++ src/main/java/com/uliana/petclinic/dao/InMemoryPetDao.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
+
 @ApplicationScoped
 public class InMemoryPetDao implements PetDao {
 
--- src/main/java/com/uliana/petclinic/model/Pet.java
+++ src/main/java/com/uliana/petclinic/model/Pet.java
@@ -5,6 +5,7 @@ import java.time.LocalDate;
 
 public class Pet implements Serializable {
 
+
     private static final long serialVersionUID = 1L;
 
     private Long id;
