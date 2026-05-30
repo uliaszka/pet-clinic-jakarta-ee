80492708ce3ba32d5ada6dc2317b63f1e92a9b41
Uliana Morozova <uliana.morozova@student.um.si>
2026-05-31 00:33:03 +0200

    implement PetBean for JSF views

--- src/main/java/com/uliana/petclinic/bean/PetBean.java
+++ src/main/java/com/uliana/petclinic/bean/PetBean.java
@@ -1,4 +1,63 @@
 package com.uliana.petclinic.bean;
 
-public class PetBean {
+import com.uliana.petclinic.dao.PetDao;
+import com.uliana.petclinic.model.Pet;
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Named("petBean")
+@ViewScoped
+public class PetBean implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+
+    @Inject
+    private PetDao petDao;
+
+    private Pet pet = new Pet();
+    private Long id;
+    private Pet selectedPet;
+
+    public List<Pet> getPets() {
+        return petDao.findAll();
+    }
+
+    public String save() {
+        petDao.create(pet);
+        pet = new Pet();
+        return "list.xhtml?faces-redirect=true";
+    }
+
+    public void loadPet() {
+        selectedPet = petDao.findById(id);
+    }
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Pet getSelectedPet() {
+        return selectedPet;
+    }
+
+    public void setSelectedPet(Pet selectedPet) {
+        this.selectedPet = selectedPet;
+    }
 }
